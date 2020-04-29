95caffa440376bfb9a0d83043d9cf932cb57fd22
Shahmir Ali <shahmirali966@gmail.com>
2020-04-29 23:59:33 +0500

    second commit

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_wtf.file import FileAllowed, FileRequired
+from wtforms.fields import (StringField, SubmitField, FileField)
+from flask_wtf import Form
+import sqlite3
 from data import Products
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret"
+
 
 Products = Products()
 
@@ -13,9 +19,19 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/rice')
+@app.route('/products', methods=["GET", "POST"])
 def products():
-    return render_template("products.html", products = Products)
+    form = UploadForm()
+    return render_template("products.html", form  = form )
+
+class UploadForm(Form):
+    file = FileField()
+    submit = SubmitField("submit")
+
+
+@app.route('/grocery')
+def grocery():
+    return render_template("grocery.html")
 
 
 if __name__ == "__main__":
